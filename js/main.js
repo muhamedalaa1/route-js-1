3e4be27b4331ea6849708930da344379b1561aa3
mohamed alaa <mohalaa1520@gmail.com>
2023-07-15 14:20:45 +0300

    js-1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var userFirstName = document.getElementById("fn");
+var userSecondName = document.getElementById("sn");
+
+function getAlertMsg(){
+
+    window.alert("hello " + userFirstName.value + " " + userSecondName.value);
+
+}
